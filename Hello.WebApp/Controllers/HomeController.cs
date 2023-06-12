ef5a1c7766674cd18bb18d7810dd45261db2c2c7
chunglv11 <info.chungnd@gmail.com>
2023-06-12 09:49:22 +0700

    Add project files.

---
+++ Hello.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hello.Application.MTbl_product;
+using Hello.WebApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Hello.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITbl_productApiClient _nhanSuApiClient;
+
+        public HomeController(ITbl_productApiClient sinhVienApiClient)
+        {
+            _nhanSuApiClient = sinhVienApiClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var response = await _nhanSuApiClient.GetAll();
+
+            var model = JsonConvert.DeserializeObject<List<Tbl_productResponse>>(response);
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(Tbl_productRequest request)
+        {
+            if (request.username == "giamdoc" && request.password == "giamdoc")
+            {
+                return Redirect("/");
+            }
+            return Redirect("/Home/Login");
+        }
+
+
+    }
+}
